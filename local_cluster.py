22e89daf061ce080d1c171c2d8b9420e166e1266
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-05 17:25:40 +0000

    t

---
+++ local_cluster.py
@@ -0,0 +1,283 @@
+from __future__ import annotations
+
+import os
+from typing import Dict
+import time
+import psutil
+import pickle
+from shlex import split
+from pprint import PrettyPrinter
+from pathlib import Path
+import json
+
+import numpy as np
+import pandas as pd
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from hdbscan import HDBSCAN
+
+
+import gemmi
+
+
+from xlib.pandda_types import (JoblibMapper, PanDDAFSModel, Datasets, Reference, 
+                                    Grid, Alignments, Shells, Xmaps, Xmap,
+                                    XmapArray, Model, Dtag, Zmaps, Clusterings,
+                                    Events, SiteTable, EventTable,
+                                    JoblibMapper, Event
+                                    )
+import joblib
+from joblib.externals.loky import set_loky_pickler
+set_loky_pickler('pickle')
+
+import plotly.graph_objects as go
+
+        
+@dataclasses.dataclass()
+class Args:
+    system_dirs_dir: Path
+    out_dir: Path
+    
+    @staticmethod
+    def from_cmd():
+        
+        fields: Tuple[dataclasses.Field, ...] = dataclasses.fields(Args)
+        parser = argparse.ArgumentParser()
+        for field in fields:
+            parser.add_argument(f"--{field.name}")
+        args = parser.parse_args()
+        args_dict = vars(args)
+        typed_args = [field.type(args_dict[field.name]) for field in fields]
+        
+        return Args.from_args(args)
+    
+    
+class Constants:
+    RESIDUE_CLUSTER_PLOT_FILE = ""
+    CLUSTERINGS_PLOT_FILE = ""
+    RECORDS_JSON_FILE = ""
+
+
+def select_partition(xmap: Xmap, partition):
+    array = xmap.to_array()
+    selection = (
+        np.array(partition_coord[0] for partition_coord in partition),
+        np.array(partition_coord[1] for partition_coord in partition),
+        np.array(partition_coord[2] for partition_coord in partition)
+                 )
+    
+    selected_values_array = array[selection]
+    
+    return selected_values_array
+
+def make_sample_by_feature_matrix(selection_dict):
+    sample_by_feature_array = np.vstack([selection for selection in selection_dict.values()])
+    
+    return sample_by_feature_array
+
+        
+def embed_pca(sample_by_feature_matrix):
+    pca_obj = PCA(n_components=50)
+    
+    pca_obj.fit_transform(sample_by_feature_matrix)
+
+
+def embed_tsne(pca_matrix):
+    tsne_obj = TSNE(n_components=2)
+    
+    tsne_obj.fit_transform(pca_matrix)
+
+def cluster(tsne_matrix):
+    hdbscan_obj = HDBSCAN()
+    
+    hdbscan_obj.fit(tsne_matrix)
+    
+    return hdbscan_obj.labels_
+
+
+def make_clustering_record(selection_dict, tsne_matrix, clustering): 
+    records = {}
+    for dtag, tsne_coords, clustering in zip(selection_dict, tsne_matrix, clustering):
+        records[dtag] = {
+            "x": tsne_coords[0],
+            "y": tsne_coords[1],
+            "cluster": clustering,
+        }
+        
+    return records
+
+def save_records(records, out_dir):
+    with open(out_dir / Constants.RECORDS_JSON_FILE, "w") as f:
+        json.dump(records, f)
+
+def plot_clustering(residue_id, tsne_matrix, clustering, out_dir):
+    fig = go.Figure()
+
+    
+    fig.add_trace(
+        go.Scatter(
+            x=tsne_matrix[:,0],
+            y=tsne_matrix[:,1],
+            mode='markers',
+            name='Original Plot',
+            marker=dict(
+                color=clustering,
+            ),
+        )
+    )
+    
+    fig.write_html(str(out_dir / Constants.CLUSTERINGS_PLOT_FILE))
+
+
+def plot_clusterings(records, out_dir: Path):
+
+    fig = go.Figure()
+    
+    # Invert dict
+    clustering_dict_dict = {}
+    for residue_id in records:
+        residue_record = records[residue_id]
+        for dtag in residue_record:
+            if dtag.dtag not in clustering_dict_dict:
+                clustering_dict_dict[dtag.dtag] = {}
+            
+            clustering_dict_dict[dtag.dtag][residue_id] = residue_record[dtag]
+
+    # Add traces to graph
+    for dtag in clustering_dict_dict:
+        series = pd.Series.from_dict(clustering_dict_dict[dtag])
+        
+        fig.add_trace(go.Scatter(
+            y=series,
+            mode='lines+markers',
+            name='Original Plot'
+        ))
+    
+    fig.write_html(str(out_dir / Constants.CLUSTERINGS_PLOT_FILE))
+
+
+def main():
+    ###################################################################
+    # # Configuration
+    ###################################################################
+    print("Getting config")
+    args = Args.from_cmd()
+    
+    print("Getting multiprocessor")
+    mapper = JoblibMapper.initialise()
+    
+    
+    ###################################################################
+    # # Pre-pandda
+    ###################################################################
+    
+    # Get datasets
+    print("Loading datasets")
+    datasets_initial: Datasets = Datasets.from_dir(args.data_dirs)
+    
+    # datasets_initial: Datasets = datasets_initial.trunate_num_datasets(100)
+
+    # Initial filters
+    print("Filtering invalid datasaets")
+    datasets_invalid: Datasets = datasets_initial.remove_invalid_structure_factor_datasets(args.structure_factors)
+
+    datasets_low_res: Datasets = datasets_invalid.remove_low_resolution_datasets(
+        args.low_resolution_completeness)
+    
+    datasets_rfree: Datasets = datasets_low_res.remove_bad_rfree(args.filtering.max_rfree)
+    
+    datasets_wilson: Datasets = datasets_rfree.remove_bad_wilson(args.max_wilson_plot_z_score)  # TODO
+    
+    # Select refernce
+    print("Getting reference")
+    reference: Reference = Reference.from_datasets(datasets_wilson)
+    
+    # Post-reference filters
+    print("smoothing")
+    start = time.time()
+    datasets_smoother: Datasets = datasets_wilson.smooth_datasets(reference, 
+                                                structure_factors=args.structure_factors,
+                                                mapper=mapper,
+                                                )
+    finish = time.time()
+    print(f"Smoothed in {finish-start}")  
+
+    print("Removing dissimilar models")
+    datasets_diss_struc: Datasets = datasets_smoother.remove_dissimilar_models(reference,
+                                                        args.max_rmsd_to_reference,
+                                                        )
+    
+    datasets_diss_space: Datasets = datasets_diss_struc.remove_dissimilar_space_groups(reference)
+
+    datasets = datasets_diss_space
+
+    # Grid
+    print("Getting grid")
+    grid: Grid = Grid.from_reference(reference,
+                            args.outer_mask,
+                                args.inner_mask_symmetry,
+                                    sample_rate=args.sample_rate,
+                                )
+
+    print("Getting alignments")
+    alignments: Alignments = Alignments.from_datasets(
+        reference,
+        datasets,
+        )
+    
+        
+    ###################################################################
+    # # Process shells
+    ###################################################################
+    sorted_dtags = list(sorted(datasets.datasets.keys(),
+                            key=lambda dtag: datasets[dtag].reflections.resolution().resolution,
+                            ))
+    
+    min_res = datasets[sorted_dtags[-1]].reflections.resolution().resolution
+
+    
+    print("Truncating datasets")
+    truncated_datasets: Datasets = datasets.truncate(resolution=min_res,
+                                                                structure_factors=args.structure_factors,
+                                                                )
+
+    # Get maps
+    xmaps = Xmaps.from_aligned_datasets_c(
+        truncated_datasets, 
+        alignments, 
+        grid,
+        args.structure_factors, 
+        sample_rate=args.sample_rate,
+        mapper=mapper,
+        ) # n x (grid size) with total_mask > 0
+    
+    records = {}
+    # Iterate over residues
+    for residue_id in reference.dataset.structure.protein_residue_ids():
+        partition = grid.partitioning[residue_id]
+        
+        selection_dict = {dtag: select_partition(xmap, partition) for dtag, xmap in xmaps.xmaps.items()}
+        
+        sample_by_feature_matrix = make_sample_by_feature_matrix(selection_dict)
+        
+        pca_matrix = embed_pca(sample_by_feature_matrix)
+        
+        tsne_matrix = embed_tsne(pca_matrix)
+        
+        clustering = cluster(tsne_matrix)
+        
+        plot_clustering(tsne_matrix, clustering, args.out_dir)
+        
+        record = make_clustering_record(selection_dict, tsne_matrix, clustering)
+        
+        records[residue_id] = record
+
+
+    plot_clusterings(records)
+    
+    save_records(records, args.out_dir)
+    
+    
+        
+        
---
+++ xlib/database_sql.py
@@ -0,0 +1,375 @@
+import os
+from pathlib import Path
+import json
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Bool, create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+import pandas as pd
+
+import xlib
+
+
+class Constants:
+    
+    PANDDA_TABLE = ""
+
+base = declarative_base()
+        
+class Compound(base):
+    __tablename__ = Constants.COMPOUND_TABLE
+    id = Column(Integer, primary_key=True)
+    path = Column(String(255))
+
+class Reflections(base):
+    __tablename__ = Constants.REFLECTIONS_TABLE
+    id = Column(Integer, primary_key=True)
+    path = Column(String(255))
+
+class Model(base):
+    __tablename__ = Constants.MODEL_TABLE
+    id = Column(Integer, primary_key=True)
+    path = Column(String(255))
+
+class Dataset(base):
+    __tablename__ = Constants.DATASET_TABLE
+    id = Column(Integer, primary_key=True)
+    dtag = Column(String(255))
+    path = Column(String(255))
+    
+    # Foriegn keys
+    reflections_id = Column(Integer, ForeignKey(Reflections.id))
+    model_id = Column(Integer, ForeignKey(Model.id))
+    compound_id = Column(Integer, ForeignKey(Compound.id))
+
+class System(base):
+    __tablename__ = Constants.SYSTEM_TABLE
+    id = Column(Integer, primary_key=True)
+    system = Column(String(255))
+    path = Column(String(255))
+
+class PanDDA(base):
+    __tablename__ = Constants.PANDDA_TABLE
+    id = Column(Integer, primary_key=True)
+    success = Column(Bool)
+    runtime = Column(Float)
+    path = Column(String(255))
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    
+class Event(base):
+    __tablename__ = Constants.EVENT_TABLE
+    id = Column(Integer, primary_key=True)
+    event_idx = Column(Integer)
+    x = Column(Float)
+    y = Column(Float)
+    z= Column(Float)
+    analysed_Resolution = Column(Float)
+    bdc = Column(Float)
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id = Column(Integer, ForeignKey(Model.id))
+    pandda_id = dataset_id = Column(Integer, ForeignKey(PanDDA.id))
+    
+
+class ReferenceModel(base):
+    __tablename__ = Constants.REFERENCE_TABLE
+    id = Column(Integer, primary_key=True)
+    path = Column(String(255))
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    
+class Autobuild(base):
+    __tablename__ = Constants.AUTOBUILD_TABLE
+    id = Column(Integer, primary_key=True)
+    path = Column(String(255))
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+
+class AutobuildBest(base):
+    __tablename__ = Constants.AUTOBUILDBEST_TABLE
+    id = Column(Integer, primary_key=True)
+    path = Column(String(255))
+
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+    autobuild_id = Column(Integer, ForeignKey(Autobuild.id))
+
+
+class AutobuildSkeletonScore(base):
+    __tablename__ = Constants.AUTOBUILDSKELETONSCORE_TABLE
+    id = Column(Integer, primary_key=True)
+    autobuild_id = Column(Integer, ForeignKey(Autobuild.id))
+    skeleton_score = Column(Float)
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+    autobuild_id = Column(Integer, ForeignKey(Autobuild.id))
+
+
+class AutobuildRSCC(base):
+    __tablename__ = Constants.AUTOBUILD_RSCC_TABLE
+    id = Column(Integer, primary_key=True)
+    rscc = Column(Float)
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+    autobuild_id = Column(Integer, ForeignKey(Autobuild.id))
+
+class AutobuildRMSD(base):
+    __tablename__ = Constants.AUTOBUILD_RMSD_TABLE
+    id = Column(Integer, primary_key=True)
+    reference_id = Column(Integer, ForeignKey(ReferenceModel.id))
+    rmsd = Column(float)
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+    autobuild_id = Column(Integer, ForeignKey(Autobuild.id))
+
+class HumanAutobuildComparison(base):
+    __tablename__ = Constants.AUTOBUILD_RMSD_TABLE
+    id = Column(Integer, primary_key=True)
+    
+    # foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+    autobuild_id = Column(Integer, ForeignKey(Autobuild.id))
+    reference_id = Column(Integer, ForeignKey(ReferenceModel.id))
+    
+class RealSpaceClustering(base):
+    __tablename__ = Constants.REAL_SPACE_CLUSTERING_TABLE
+    id = Column(Integer, primary_key=True)
+    table_path = Column(String(255))
+    graph_path = path = Column(String(255))
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+
+class EventScore(base):
+    __tablename__ = Constants.EVENT_SCORE_TABLE
+    id = Column(Integer, primary_key=True)
+    event_score = Column(Float)
+    
+    # Foreign keys
+    system_id = Column(Integer, ForeignKey(System.id))
+    dataset_id= Column(Integer, ForeignKey(Dataset.id))
+    pandda_id= Column(Integer, ForeignKey(PanDDA.id))
+    event_id= Column(Integer, ForeignKey(Event.id))
+
+
+class Database:
+    
+    def __init__(self, database_path: Path, overwrite: bool = False) -> None:
+        super().__init__()
+        # conn = sqlite3.connect('example.db')
+        if overwrite: 
+            os.rm(str(database_path))
+        
+        engine = create_engine(f'sqlite:///{str(database_path)}')
+        
+        
+        base.metadata.bind = engine
+        base.metadata.create_all(engine)
+        
+        DBSession = sessionmaker()
+        DBSession.bind = engine
+        session = DBSession()
+        
+    def populate_systems(self, system_dir: Path):
+        system_path_list: List[Path] = list(path for path in system_dir.glob("*") if path.is_dir())
+        
+        for system_path in system_path_list:
+            system_name: str = system_path.name
+            system_path: str =str(system_path) 
+        
+            system = System(
+                system=system_name, 
+                path=system_path,
+                )
+            self.session.add(system)
+        
+        self.session.commit()
+        
+    def populate_models_reflections_compounds_datasets(self):
+        
+        for system in self.session.query(System):
+            system_path = Path(system.path)
+            
+            dataset_dirs_dir = system_path / system.system
+            
+            dataset_dir_list = list(path for path in dataset_dirs_dir.glob("*") if path.is_dir())
+            
+            for dataset_dir in dataset_dir_list:
+                compound_path = dataset_dir / Constants.COMPOUND_FILE
+                reflections_path = dataset_dir / Constants.REFLECTIONS_FILE
+                model_path = dataset_dir / Constants.MODEL_FILE
+            
+                model = Model(path=str(compound_path))
+            
+                reflections = Reflections(path=str(reflections_path))
+                
+                compound = Compound(path=str(model_path))
+                
+                dataset = Dataset(dtag=dataset_dir.name,
+                                  path=str(dataset_dir),
+                                  reflections=reflections,
+                                  compound=compound,
+                                  model=model,
+                                  )
+                
+                self.session.add(model)
+                self.session.add(reflections)
+                self.session.add(compound)
+                self.session.add(dataset)
+                
+        self.session.commit()
+        
+    def populate_panddas(self, pandda_dirs_dir: Path):
+        
+        for system in self.session.query(System):
+            pandda_dir = pandda_dir / system.system
+            
+            pandda_json_file = pandda_dir / xlib.Constants.PANDDA_LOG_FILE
+            
+            with open(pandda_json_file, "r") as f:
+                pandda_json = json.load(f)
+        
+            pandda = PanDDA(
+                path=str(pandda_dir),
+                system=system,
+                success=pandda_json["success"],
+                runtime=pandda_json["runtime"],
+                )
+            self.session.add(pandda)
+        
+        self.session.commit()
+        
+    def populate_autobuilds(self, autobuild_dir: Path):
+        
+        build_dict: xlib.BuildDict = xlib.BuildDict.from_autobuild_dir(autobuild_dirs_dir)
+        
+        for build_id in build_dict:
+            build: xlib.Build = build_dict[build_id]
+            
+            system = self.session.query(System).filter(System.system == build_id.system.system).first()
+            pandda = self.session.query(PanDDA).filter(PanDDA.system == build_id.system.system).first()
+            dataset = self.session.query(Dataset).filter(Dataset.dtag == build_id.dtag.dtag ).first()
+            event = self.session.query(Event).filter(Event.system.system == build_id.system.system &
+                                                     Event.dataset.dtag == build_id.dtag.dtag &
+                                                     Event.event_idx == build_id.event_idx.event_idx
+                                                     ).first()
+            
+            
+            autobuild = Autobuild(path=str(build.build_file),
+                                  system=system,
+                                  dataset=dataset,
+                                  pandda=pandda,
+                                  event=event,
+                                  )    
+            
+            self.session.add(autobuild)
+            
+    def populate_events(self):
+        
+        for pandda in self.session.query(PanDDA):
+            pandda_dir = Path(pandda.path)
+            pandda_analyses_dir = pandda_dir / xlib.Constants.PANDDA_ANALYSES_DIR
+            pandda_event_table_file = pandda_analyses_dir / xlib.Constants.PANDDA_ANALYSE_EVENTS_FILE
+            
+            # Get table
+            pandda_event_table: pd.DataFrame = pd.read_csv(str(pandda_event_table_file))
+            
+            # Get events
+            for index, row in pandda_event_table.iterrows():
+                system = pandda.system
+                dataset = self.session.query(Dataset).filter(Dataset.dtag == row["dtag"]).first()
+                
+                event_dir = pandda_dir/ xlib.Constants.PANDDA_PROCESSED_DATASETS_DIR / dataset.dtag
+                
+                
+                event = Event(
+                    event_idx = row["event_idx"],
+                    bdc = row["1-BDC"],
+                    x = row["x"],
+                    y = row["y"],
+                    z = row["z"],
+                    analysed_Resolution = row["analysed_resolution"],
+                    system=system,
+                    dataset=dataset,
+                    pandda=pandda,
+                    )
+                            
+                self.session.add(event)
+        
+        self.session.commit()
+        
+        
+@dataclasses.dataclass()
+class Args:
+    system_dirs_dir: Path
+    pandda_dirs_dir: Path
+    autobuild_dirs_dir: Path
+    database_file: Path
+    
+    @staticmethod
+    def from_cmd():
+        
+        fields: Tuple[dataclasses.Field, ...] = dataclasses.fields(Args)
+        parser = argparse.ArgumentParser()
+        for field in fields:
+            parser.add_argument(f"--{field.name}")
+        args = parser.parse_args()
+        
+        args_dict = vars(args)
+        
+        typed_args = [field.type(args_dict[field.name]) for field in fields]
+        
+        return Args.from_args(args)
+        
+def main():
+    args = Args.from_cmd()
+    database = Database(
+        args.database_file, 
+        overwrite=True,
+        )
+    
+    database.populate_systems(args.system_dirs_dir)
+    database.populate_models_reflections_compounds_datasets()
+    database.populate_panddas(args.pandda_dirs_dir)
+    database.populate_autobuilds(args.autobuild_dirs_dir)
+    database.populate_events()
+    
+    database.populate_reference_models()
+    database.populate_autobuild_rmsds()
+    database.populate_autobuild_rsccs()
+    database.populate_autobuild_skeleton_scores()
+    database.populate_human_autobuild_comparison()
+    database.populate_real_space_clusterings()
+    database.populate_event_scores()
+    
+if __name__ == "__main__":
+    # If run as a script creates and populates database
+    main()
