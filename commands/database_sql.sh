52179de5a5733073f35df5e876c4eecade56fc4a
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-08 10:35:50 +0000

    t

--- commands/database_sql.sh
+++ commands/database_sql.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-conda activate env_rdkit
-
-python /data/share-2/conor/pandda/pandda_scripts/database/database_sql.py --system_dirs_dir=/data/share-2/conor/pandda/data/pandda_inputs/ --pandda_dirs_dir=/data/share-2/conor/pandda/output/all_systems/ --autobuild_dirs_dir=/data/share-2/conor/pandda/output/all_autobuild/ --reference_model_dir=/data/share-2/conor/pandda/data/reference_structures
+python /data/share-2/conor/pandda/pandda_scripts/database/database_sql.py --system_dirs_dir=/data/share-2/conor/pandda/data/pandda_inputs/ --pandda_dirs_dir=/data/share-2/conor/pandda/output/all_systems/ --autobuild_dirs_dir=/data/share-2/conor/pandda/output/all_autobuild/ --reference_model_dir=/data/share-2/conor/pandda/data/reference_structures --database_file=/data/share-2/conor/pandda/output/database.sqlite
 
