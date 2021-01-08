c169d9ba26c2653262f9bb4d9aec2b883d96d5f9
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-08 10:55:40 +0000

    t

---
+++ commands/batch_pandda_monitor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python pandda_scripts/batch_pandda_monitor.py --pandda_dirs_dir=/data/share-2/conor/pandda/output/all_pandda/ --autobuild_dirs_dir=/data/share-2/conor/pandda/output/ --graph_dir=/data/share-2/conor/pandda/graphs/ --analyses_dir=/data/share-2/conor/pandda/output/all_analyses
+
