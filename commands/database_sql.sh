f60f909a55f2eca675811ff4bfeb0637ca19a8f9
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-08 17:36:36 +0000

    t

--- commands/database_sql.sh
+++ commands/database_sql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python /data/share-2/conor/pandda/pandda_scripts/database/database_sql.py --system_dirs_dir=/data/share-2/conor/pandda/data/pandda_inputs/ --pandda_dirs_dir=/data/share-2/conor/pandda/output/all_systems/ --autobuild_dirs_dir=/data/share-2/conor/pandda/output/all_autobuild/ --reference_model_dir=/data/share-2/conor/pandda/data/reference_structures --database_file=/data/share-2/conor/pandda/output/database.sqlite
+python /data/share-2/conor/pandda/pandda_scripts/xlib/xlib/database_sql.py --system_dirs_dir=/data/share-2/conor/pandda/data/pandda_inputs/ --pandda_dirs_dir=/data/share-2/conor/pandda/output/all_systems/ --autobuild_dirs_dir=/data/share-2/conor/pandda/output/all_autobuild/ --reference_model_dir=/data/share-2/conor/pandda/data/reference_structures --database_file=/data/share-2/conor/pandda/output/database.sqlite
 
