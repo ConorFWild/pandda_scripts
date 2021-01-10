37e1f52df12ce32d4ae069b24c2a00af152db961
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-10 19:31:59 +0000

    t

--- commands/batch_rhofit_clemens.sh
+++ commands/batch_rhofit_clemens.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 python /data/share-2/conor/pandda/pandda_scripts/batch_rhofit_clemens.py --pandda_dirs_dir=/data/share-2/conor/pandda/output/all_pandda/ --autobuild_dirs_dir=/data/share-2/conor/pandda/output/all_autobuild/
