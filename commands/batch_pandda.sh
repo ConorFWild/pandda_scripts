237c8258c91bf800de2ca059648ad6c67d0d883f
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-13 12:29:45 +0000

    t

--- commands/batch_pandda.sh
+++ commands/batch_pandda.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python pandda_scripts/batch_pandda.py --data_dirs_dir=/data/share-2/conor/pandda/data/pandda_inputs/ --out_dirs_dir=/data/share-2/conor/pandda/output/all_pandda/ --request_memory=400
+python /data/share-2/conor/pandda/pandda_scripts/batch_pandda.py --data_dirs_dir=/data/share-2/conor/pandda/data/pandda_inputs/ --out_dirs_dir=/data/share-2/conor/pandda/output/all_pandda/ --request_memory=400
