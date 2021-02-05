8965119ae0d9add396d99708ff17ae2651bd777a
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-02-05 12:30:29 +0000

    t

--- commands/batch_cluster.sh
+++ commands/batch_cluster.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-python /data/share-2/conor/pandda/pandda_scripts/batch_cluster.py --data_dirs=/data/share-2/conor/pandda/output/all_pandda/ --out_dirs=/data/share-2/conor/pandda/output/all_cluster
- 
+python /data/share-2/conor/pandda/pandda_scripts/batch_cluster.py --data_dirs=/data/share-2/conor/pandda/data/pandda_inputs/ --out_dirs=/data/share-2/conor/pandda/output/all_cluster
