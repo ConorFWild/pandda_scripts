bd625aac528530dd629ed9f948a82a45cc32ccbd
cwild <conor.wild@sky.com>
2021-04-15 14:50:12 +0100

    y

--- scripts/pandda_local_cluster_XX02KALRNA.sh
+++ scripts/pandda_local_cluster_XX02KALRNA.sh
@@ -13,9 +13,11 @@ local_pandda_cluster_script="$root/pandda_local_cluster/pandda_local_cluster.py"
 mkdir $output_dir
 
 echo -e "#!/bin/bash \n/data/share-2/conor/anaconda3/envs/lab/bin/python $local_pandda_cluster_script $data_dirs $output_dir" > $submit_script
+echo -e "# Executable script at $submit_script"
 
 chmod 777 $submit_script
 
 echo -e "Executable = $submit_script \noutput = $output_file \nerror = $error_file \nlog = $log_file \nrequest_memory = 100 GB \nQueue" > $job_script
+echo -e "# Job script at $job_script"
 
 condor_submit $job_script
