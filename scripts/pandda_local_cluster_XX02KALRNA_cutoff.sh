887647f368ef5b464cda1329680f51969eddb740
cwild <conor.wild@sky.com>
2021-04-19 11:47:37 +0100

    y

--- scripts/pandda_local_cluster_XX02KALRNA_cutoff.sh
+++ scripts/pandda_local_cluster_XX02KALRNA_cutoff.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cutoff="$1"
+cutoff=$1
 root="/data/share-2/conor/pandda"
-data_dirs="$root/data/pandda_inputs/XX02KALRNA_$1"
-output_dir="$root/output/test_local_cluster_XX02KALRNA_$1"
-submit_script="$output_dir/test_local_cluster_XX02KALRNA_$1.sh"
-job_script="$output_dir/test_local_cluster_XX02KALRNA_$1.job"
-output_file="$output_dir/test_local_cluster_XX02KALRNA_$1.out"
-error_file="$output_dir/test_local_cluster_XX02KALRNA_$1.err"
-log_file="$output_dir/test_local_cluster_XX02KALRNA_$1.log"
+data_dirs="$root/data/pandda_inputs/XX02KALRNA_$cutoff"
+output_dir="$root/output/test_local_cluster_XX02KALRNA_$cutoff"
+submit_script="$output_dir/test_local_cluster_XX02KALRNA_$cutoff.sh"
+job_script="$output_dir/test_local_cluster_XX02KALRNA_$cutoff.job"
+output_file="$output_dir/test_local_cluster_XX02KALRNA_$cutoff.out"
+error_file="$output_dir/test_local_cluster_XX02KALRNA_$cutoff.err"
+log_file="$output_dir/test_local_cluster_XX02KALRNA_$cutoff.log"
 local_pandda_cluster_script="$root/pandda_local_cluster/pandda_local_cluster.py"
 
 
 mkdir $output_dir
 
-echo -e "#!/bin/bash \n/data/share-2/conor/anaconda3/envs/lab/bin/python $local_pandda_cluster_script $data_dirs $output_dir --cutoff=$1" > $submit_script
+echo -e "#!/bin/bash \n/data/share-2/conor/anaconda3/envs/lab/bin/python $local_pandda_cluster_script $data_dirs $output_dir --cutoff=$cutoff" > $submit_script
 echo -e "# Executable script at $submit_script"
 
 chmod 777 $submit_script
